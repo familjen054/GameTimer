26cd57702b07f9f6081e286e6a3b23208611916a
familjen054 <familjen054@gmail.com>
2024-07-07 23:05:24 +0200

    Add files via upload

--- GameTimer.py
+++ GameTimer.py
@@ -1,20 +1,7 @@
-import pygame
+import cv2
+import numpy as np
 
-pygame.init()
-screen = pygame.display.set_mode((400, 300))
-done = False
-white=(255,255,255)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
+display = 200 * np.ones([400, 400, 3], dtype=np.uint8)
 
-bg = (127,127,127)
-while not done:
-   for event in pygame.event.get():
-      screen.fill(bg)
-      if event.type == pygame.QUIT:
-         done = True
-      font = pygame.font.SysFont("Arial", 36)
-   txtsurf = font.render("Hello, World", True, white)
-   screen.blit(txtsurf,(200 - txtsurf.get_width() // 2, 150 - txtsurf.get_height() // 2))
-   pygame.display.update()
+cv2.imshow("", display)
+cv2.waitKey(0)
